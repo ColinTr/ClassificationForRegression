5792f950d2837259cd7821b51e157a10d45b6fd6
Colin Troisemaine <troc3202@usherbrooke.ca>
2021-05-05 08:36:00 -0700

    Added dataset importation and empty preprocessing methods

--- src/DataProcessingUtils.py
+++ src/DataProcessingUtils.py
@@ -4,3 +4,47 @@ Authors : Colin Troisemaine & Vincent Lemaire
 contact : colin.troisemaine@gmail.com
 """
 
+
+def one_hot_encode():
+    # TODO
+    return None
+
+
+def apply_box_cox():
+    # TODO
+    return None
+
+
+def standardize():
+    # TODO
+    return None
+
+
+def equal_width_split():
+    # TODO
+    return None
+
+
+def equal_freq_split():
+    # TODO
+    return None
+
+
+def below_threshold_class_gen():
+    # TODO
+    return None
+
+
+def inside_bin_class_gen():
+    # TODO
+    return None
+
+
+def kfold_train_test_split():
+    # TODO
+    return None
+
+
+def save_dataframe_to_file():
+    # TODO
+    return None
--- src/scripts/data_processing.py
+++ src/scripts/data_processing.py
@@ -5,16 +5,26 @@ contact : colin.troisemaine@gmail.com
 """
 
 import argparse
+import pandas as pd
+import time
 
 
 def argument_parser():
+    """
+    A parser to allow user to easily process any dataset using the split method and class generation he desires.
+    """
+
     parser = argparse.ArgumentParser(usage='\n python3 data_processing.py [dataset_path] [output_path] [split_method] '
-                                           '[output_classes]',
+                                           '[output_classes] [delimiter] [header] [decimal] [na_values]'
+                                           '\n Example : python scripts/data_processing.py '
+                                           '--dataset_path=\"../data/raw/3D_Road_Network_Dataset/3D_spatial_network'
+                                           '.csv\"',
                                      description="This program allows to process datasets to be used later.")
 
     parser.add_argument('--dataset_path',
                         type=str,
-                        help='The dataset to process')
+                        help='The dataset to process',
+                        required=True)
 
     parser.add_argument('--output_path',
                         type=str,
@@ -33,12 +43,41 @@ def argument_parser():
                         choices=["below_threshold", "inside_bin"],
                         help='The method of class generation')
 
+    parser.add_argument('--delimiter',
+                        type=str,
+                        default=",",
+                        help='Delimiter to use when reading the dataset')
+
+    parser.add_argument('--header',
+                        type=str,
+                        default="infer",
+                        choices=["infer", "None"],
+                        help='Infer the column names or use None if the first line isn\' a csv header line')
+
+    parser.add_argument('--decimal',
+                        type=str,
+                        default=",",
+                        help='Character to recognize as decimal point')
+
+    parser.add_argument('--na_values',
+                        type=str,
+                        help='Additional string to recognize as NA/NaN')
+
     return parser.parse_args()
 
 
 if __name__ == "__main__":
-    """
-    """
     args = argument_parser()
+
+    print("Reading the dataset's file...")
+    reading_start_time = time.time()
+    imported_dataset = pd.read_csv(args.dataset_path, delimiter=args.delimiter, header=args.header,
+                                   decimal=args.decimal, na_values=args.na_values)
+    print("Dataset imported ({0:.2f}".format(time.time() - reading_start_time) + "sec)")
+
+    print("\nDataset's first 3 rows :")
+    print(imported_dataset.head(3))
+
+    # TODO
 
     pass
