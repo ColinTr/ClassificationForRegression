a11eb0b577cbb9c70b78c0be7c982241d982d251
Colin Troisemaine <troc3202@usherbrooke.ca>
2021-05-06 07:15:34 -0700

    Implemented kfold, discretization and csv files saving

--- src/DataProcessingUtils.py
+++ src/DataProcessingUtils.py
@@ -4,6 +4,9 @@ Authors : Colin Troisemaine & Vincent Lemaire
 contact : colin.troisemaine@gmail.com
 """
 
+import numpy as np
+import random
+
 
 def one_hot_encode():
     # TODO
@@ -20,31 +23,91 @@ def standardize():
     return None
 
 
-def equal_width_split():
-    # TODO
-    return None
+def equal_width_split(Y, n_bins):
+    """
+    TODO
+    :param Y:
+    :param n_bins:
+    :return:
+    """
+    Y_min = np.min(Y)
+    Y_max = np.max(Y)
+    step = (Y_max - Y_min) / n_bins
+
+    thresholds_list = [(Y_min + index * step) for index in range(1, n_bins)]
+
+    return thresholds_list
 
 
-def equal_freq_split():
-    # TODO
-    return None
+def equal_freq_split(Y, n_bins):
+    """
+    TODO
+    :param Y:
+    :param n_bins:
+    :return:
+    """
+    thresholds_list = []
+
+    split_sorted_Y = np.array_split(sorted(Y), n_bins)
+
+    # We define the thresholds as the mean between the last element of a list and the first of the next list
+    for index in range(len(split_sorted_Y)):
+        if index < len(split_sorted_Y) - 1:  # We wont have to compute a threshold for the last bin
+            threshold = (split_sorted_Y[index][-1] + split_sorted_Y[index + 1][0]) / 2
+            thresholds_list.append(threshold)
+
+    return thresholds_list
 
 
-def below_threshold_class_gen():
-    # TODO
-    return None
+def below_threshold_class_gen(Y, thresholds_list):
+    """
+    TODO
+    :param Y: numpy array
+    :param thresholds_list:
+    :return:
+    """
+    classes = np.zeros((len(Y), len(thresholds_list)), dtype=int)
+    for threshold_index, threshold in zip(range(len(thresholds_list)), thresholds_list):
+        for i in range(len(Y)):
+            if Y[i] < threshold:
+                classes[i][threshold_index] = 1
+
+    return classes
 
 
-def inside_bin_class_gen():
-    # TODO
-    return None
+def inside_bin_class_gen(Y, thresholds_list):
+    """
+    TODO
+    :param Y:
+    :param thresholds_list:
+    :return:
+    """
+
+    thresholds_pairs = [[np.min(Y), thresholds_list[0]]]
+    for threshold_index in range(0, len(thresholds_list) - 1):
+        thresholds_pairs.append([thresholds_list[threshold_index], thresholds_list[threshold_index + 1]])
+    thresholds_pairs.append([thresholds_list[-1], np.max(Y)])
+
+    classes = []
+    for value in np.array(Y):
+        # If the value is the maximum of the range, its class is the last one
+        if value == np.max(Y):
+            classes.append(len(thresholds_pairs) - 1)
+        else:
+            # Otherwise we iterate through the bins :
+            for class_index in range(0, len(thresholds_pairs)):
+                # Notice the <= for the first element, which is why we needed to check for the maximum value earlier
+                if thresholds_pairs[class_index][0] <= value < thresholds_pairs[class_index][1]:
+                    classes.append(class_index)
+
+    return classes
 
 
-def kfold_train_test_split():
-    # TODO
-    return None
+def kfold_train_test_split(n_indexes, k_folds, seed=None):
+    random.seed(seed)
 
+    indexes_list = list(range(0, n_indexes))
 
-def save_dataframe_to_file():
-    # TODO
-    return None
+    random.shuffle(indexes_list)
+
+    return np.array_split(indexes_list, k_folds)
---
+++ src/data_processing.py
@@ -0,0 +1,217 @@
+"""
+Orange Labs
+Authors : Colin Troisemaine & Vincent Lemaire
+contact : colin.troisemaine@gmail.com
+"""
+
+from DataProcessingUtils import *
+from scipy.stats import boxcox
+import pandas as pd
+import argparse
+import logging
+import ntpath
+import time
+
+
+def argument_parser():
+    """
+    A parser to allow user to easily process any dataset using the split method and class generation he desires.
+    """
+
+    parser = argparse.ArgumentParser(usage='\n python3 data_processing.py [dataset_path] [output_path] [split_method] '
+                                           '[output_classes] [delimiter] [header] [decimal] [na_values] [usecols] '
+                                           '[goal_var_index] [n_bins] [k_folds] [log_lvl]'
+                                           '\n Example : python scripts/data_processing.py '
+                                           '--dataset_path=\"../data/raw/3D_Road_Network_Dataset/3D_spatial_network'
+                                           '.csv\"',
+                                     description="This program allows to process datasets to be used later.")
+
+    parser.add_argument('--dataset_path',
+                        type=str,
+                        help='The dataset to process',
+                        required=True)
+
+    parser.add_argument('--output_path',
+                        type=str,
+                        help='The folder where the result will be written',
+                        required=True)
+
+    parser.add_argument('--split_method',
+                        type=str,
+                        default="equal_freq",
+                        choices=["equal_width", "equal_freq", "kmeans"],
+                        help='The splitting method to use')
+
+    parser.add_argument('--output_classes',
+                        type=str,
+                        default="below_threshold",
+                        choices=["below_threshold", "inside_bin"],
+                        help='The method of class generation')
+
+    parser.add_argument('--delimiter',
+                        type=str,
+                        default=",",
+                        help='Delimiter to use when reading the dataset')
+
+    parser.add_argument('--header',
+                        type=str,
+                        default="infer",
+                        choices=["infer", "None"],
+                        help='Infer the column names or use None if the first line isn\' a csv header line')
+
+    parser.add_argument('--decimal',
+                        type=str,
+                        default=",",
+                        help='Character to recognize as decimal point')
+
+    parser.add_argument('--na_values',
+                        type=str,
+                        help='Additional string to recognize as NA/NaN')
+
+    parser.add_argument('--usecols',
+                        type=int,
+                        nargs='*',  # 0 or more values expected
+                        help='The indexes of the columns to keep')
+
+    parser.add_argument('--goal_var_index',
+                        type=int,
+                        required=True,
+                        help='The index of the column to use as the goal variable')
+
+    parser.add_argument('--n_bins',
+                        type=int,
+                        default=10,
+                        help='The number of bins to create')
+
+    parser.add_argument('--k_folds',
+                        type=int,
+                        default=10,
+                        help='The number of folds in the k-folds')
+
+    parser.add_argument('--log_lvl',
+                        type=str,
+                        default='warning',
+                        choices=["debug", "info", "warning"],
+                        help='Change the log display level')
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = argument_parser()
+
+    # Get the current configuration
+    dataset_path = args.dataset_path
+    output_path = args.output_path
+    split_method = args.split_method
+    output_classes = args.output_classes
+    delimiter = args.delimiter
+    header = args.header
+    decimal = args.decimal
+    na_values = args.na_values
+    usecols = args.usecols
+    goal_var_index = args.goal_var_index
+    n_bins = args.n_bins
+    k_folds = args.k_folds
+    if args.log_lvl == 'debug':
+        logging.getLogger().setLevel(logging.DEBUG)
+    elif args.log_lvl == 'info':
+        logging.getLogger().setLevel(logging.INFO)
+    elif args.log_lvl == 'warning':
+        logging.getLogger().setLevel(logging.WARNING)
+    else:
+        raise ValueError('Unknown parameter for log_lvl.')
+
+    logging.info("Reading the dataset's file...")
+    reading_start_time = time.time()
+    imported_dataset = pd.read_csv(dataset_path, delimiter=delimiter, header=header, decimal=decimal,
+                                   na_values=na_values, usecols=usecols)
+    logging.info("Dataset imported ({0:.2f}".format(time.time() - reading_start_time) + "sec)")
+
+    # We keep all the columns except the goal variable one
+    X = imported_dataset.drop(imported_dataset.columns[goal_var_index], axis=1)
+    Y = imported_dataset[imported_dataset.columns[goal_var_index]]
+
+    logging.debug("\nDataset's first 3 rows :")
+    logging.debug("X :")
+    logging.debug(X.head(3))
+    logging.debug("\nY :")
+    logging.debug(Y.head(3))
+
+    # TODO : Categorical data encoding => Maybe do that in another script (or even a notebook)
+
+    k_fold_indexes = kfold_train_test_split(len(Y), k_folds)
+
+    for k_fold_index in range(0, k_folds):
+        logging.debug("========== FOLD N°" + str(k_fold_index) + " ==========")
+
+        train_indexes = []
+        test_indexes = []
+
+        for group in range(k_folds):
+            if group == k_fold_index:
+                test_indexes.append(k_fold_indexes[group])
+            else:
+                train_indexes.append(k_fold_indexes[group])
+
+        train_indexes = np.concatenate(train_indexes).ravel()  # Ravel the list of lists of indexes
+        test_indexes = np.concatenate(test_indexes).ravel()
+
+        X_train, Y_train = X.iloc[train_indexes].copy(), np.array(Y.iloc[train_indexes])
+        X_test, Y_test = X.iloc[test_indexes].copy(), np.array(Y.iloc[test_indexes])
+
+        # ======================= BoxCox =======================
+        # TODO : Fit BoxCox on the training dataset
+        # TODO : Apply BoxCox on the training AND testing datasets
+        # ======================================================
+
+        # =================== Normalization ====================
+        # TODO : Fit normalization on the training dataset
+        # TODO : Apply normalization on the training AND testing datasets
+        # ======================================================
+
+        # =================== discretization ===================
+        # Thresholds definition (= Fit on the training data)
+        thresholds_list = None
+        if split_method == "equal_width":
+            thresholds_list = equal_width_split(Y_train, n_bins)
+        elif split_method == "equal_freq":
+            thresholds_list = equal_freq_split(Y_train, n_bins)
+        elif split_method == "kmeans":
+            # (optional) TODO
+            raise ValueError('This split method has not been implemented yet.')
+        else:
+            raise ValueError('Unknown parameter for split_method.')
+        logging.debug("\nThresholds :" + str(thresholds_list))
+
+        # Discretization
+        train_discretized_classes, test_discretized_classes = None, None
+        if output_classes == "below_threshold":
+            train_discretized_classes = below_threshold_class_gen(Y_train, thresholds_list)
+            test_discretized_classes = below_threshold_class_gen(Y_test, thresholds_list)
+        elif output_classes == "inside_bin":
+            train_discretized_classes = inside_bin_class_gen(Y_train, thresholds_list)
+            test_discretized_classes = below_threshold_class_gen(Y_test, thresholds_list)
+        else:
+            raise ValueError('Unknown parameter for output_classes.')
+        logging.debug("\nGenerated classes (for train dataset) :\n" + str(pd.DataFrame(train_discretized_classes).head(5)))
+        # ======================================================
+
+        # We then add the generated classes to the dataframe
+        for class_index in range(len(train_discretized_classes[1])):
+            X_train['class_' + str(class_index)] = train_discretized_classes[:, class_index]
+            X_test['class_' + str(class_index)] = test_discretized_classes[:, class_index]
+
+        logging.debug("\nFinal dataframe (train) :\n" + str(X_train.head(3)))
+
+        # TODO : Save the result in a CSV file
+        final_dataframe = pd.DataFrame([])
+        # We generate the filename while making sure that we don't add too many '/'
+        file_prefix = output_path + ('/' if output_path[-1] == '' else '/') + 'fold_' + str(k_fold_index)
+        train_output_name = file_prefix + '_TRAIN_' + ntpath.basename(dataset_path)
+        X_train.to_csv(path_or_buf=train_output_name)
+
+        test_output_name = file_prefix + '_TEST_' + ntpath.basename(dataset_path)
+        X_test.to_csv(path_or_buf=test_output_name)
+
+        logging.info("Split " + str(k_fold_index) + " datasets saved in files")
