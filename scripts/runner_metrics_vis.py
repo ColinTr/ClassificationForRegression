2d585a297bd9302ead62ad0b5e1e2f6a04283316
ColinTr <>
2021-07-06 15:38:47 +0200

    Updated runner_metrics_vis.py to sort the execution order

--- scripts/runner_metrics_vis.py
+++ scripts/runner_metrics_vis.py
@@ -22,21 +22,20 @@ if __name__ == "__main__":
     grid_search = 'True'
 
     datasets_directories = [f.path for f in os.scandir(os.path.join('..', 'data', 'predictions')) if f.is_dir()]
-    datasets_names = [dataset_directory.split(os.path.sep)[-1] for dataset_directory in datasets_directories]
-    datasets_names = sorted(datasets_names)
+    datasets_names = sorted([dataset_directory.split(os.path.sep)[-1] for dataset_directory in datasets_directories])
 
     cmd_list = []
     for dataset_name in datasets_names:
         bins_level_directories = [f.path for f in os.scandir(os.path.join('..', 'data', 'predictions', dataset_name)) if f.is_dir()]
-        bins_level_directories = [bins_level_directory.split(os.path.sep)[-1] for bins_level_directory in bins_level_directories]
+        bins_level_directories = sorted([bins_level_directory.split(os.path.sep)[-1] for bins_level_directory in bins_level_directories])
 
         for bins_level_directory in bins_level_directories:
             classifier_level_directories = [f.path for f in os.scandir(os.path.join('..', 'data', 'predictions', dataset_name, bins_level_directory)) if f.is_dir()]
-            classifier_level_directories = [classifier_level_directory.split(os.path.sep)[-1] for classifier_level_directory in classifier_level_directories]
+            classifier_level_directories = sorted([classifier_level_directory.split(os.path.sep)[-1] for classifier_level_directory in classifier_level_directories])
 
             for classifier_level_directory in classifier_level_directories:
                 regressor_level_directories = [f.path for f in os.scandir(os.path.join('..', 'data', 'predictions', dataset_name, bins_level_directory, classifier_level_directory)) if f.is_dir()]
-                regressor_level_directories = [regressor_level_directory.split(os.path.sep)[-1] for regressor_level_directory in regressor_level_directories]
+                regressor_level_directories = sorted([regressor_level_directory.split(os.path.sep)[-1] for regressor_level_directory in regressor_level_directories])
 
                 for regressor_level_directory in regressor_level_directories:
                     cmd_list.append('python compute_metrics.py --predictions_folder=\"{}\"'
