9cbeecc7d5664ac78c16960868195e98a715c2c6
ColinTr <56585263+ColinTr@users.noreply.github.com>
2021-07-12 06:50:06 -0700

    Better critical_diagram.py script

--- scripts/critical_diagram.py
+++ scripts/critical_diagram.py
@@ -17,28 +17,22 @@ if __name__ == "__main__":
 
     # The paths to the RMSE table where the results are stored. At least two tables must be given to be compared
     rmse_tables = [
-        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_DecisionTree_regressor', 'RMSE_table.csv'), str(n_bins) + ' T Test RMSE'),
-        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_DecisionTree_regressor', 'RMSE_table.csv'), 'Base Test RMSE'),
-        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_LinearRegression_regressor', 'RMSE_table.csv'), str(n_bins) + ' T Test RMSE'),
-        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_LinearRegression_regressor', 'RMSE_table.csv'), 'Base Test RMSE'),
-        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_RandomForest_regressor', 'RMSE_table.csv'), str(n_bins) + ' T Test RMSE'),
-        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_RandomForest_regressor', 'RMSE_table.csv'), 'Base Test RMSE'),
-        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_Khiops_classifier_Khiops_regressor', 'RMSE_table.csv'), 'Base Test RMSE'),
-        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_Khiops_classifier_Khiops_regressor', 'RMSE_table.csv'), str(n_bins) + ' T Test RMSE')
+        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_DecisionTree_regressor', 'RMSE_table.csv'), str(n_bins) + ' T Test RMSE', 'DT+'),
+        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_DecisionTree_regressor', 'RMSE_table.csv'), 'Base Test RMSE', 'DT'),
+        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_LinearRegression_regressor', 'RMSE_table.csv'), str(n_bins) + ' T Test RMSE', 'LR+'),
+        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_LinearRegression_regressor', 'RMSE_table.csv'), 'Base Test RMSE', 'LR'),
+        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_RandomForest_regressor', 'RMSE_table.csv'), str(n_bins) + ' T Test RMSE', 'RF+'),
+        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_RandomForest_regressor', 'RMSE_table.csv'), 'Base Test RMSE', 'RF'),
+        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_Khiops_classifier_Khiops_regressor', 'RMSE_table.csv'), 'Base Test RMSE', 'Khiops'),
+        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_Khiops_classifier_Khiops_regressor', 'RMSE_table.csv'), str(n_bins) + ' T Test RMSE', 'Khiops+')
     ]
-
-    # The labels corresponding to the paths (in the same order)
-    labels = ['DT+', 'DT', 'LR+', 'LR', 'RF+', 'RF', 'Khiops', 'Khiops+']
 
     # The method that is compared with other methods If omitted, show pairwise comparison of methods
     cdmethod = None
 
-    if len(labels) != len(rmse_tables):
-        raise Exception("Please define as many labels as there are rmse_tables")
-
     groups = []
     # Get for each table the column corresponding to the parameters
-    for path, column in rmse_tables:
+    for path, column, _ in rmse_tables:
         tmp_df = pd.read_csv(path)
         groups.append(list(tmp_df[column]))
 
@@ -48,5 +42,5 @@ if __name__ == "__main__":
     average_ranks = [np.mean(vect) for vect in np.array(ranked_data).T]
 
     cd = Orange.evaluation.compute_CD(average_ranks, len(groups[0]))
-    Orange.evaluation.graph_ranks(average_ranks, labels, cd=cd, width=6, textspace=1.5, cdmethod=cdmethod)
+    Orange.evaluation.graph_ranks(average_ranks, [vect[2] for vect in rmse_tables], cd=cd, width=6, textspace=1.5, cdmethod=cdmethod)
     plt.savefig('critical_diagram_' + str(n_bins) + 'bins.png')
