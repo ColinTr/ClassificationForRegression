6b05aaba634f54e1042695124082671d3a9bee2e
Colin Troisemaine <{ID}+{username}@users.noreply.github.com>
2021-07-19 11:51:26 +0200

    Fixed critical_diagram.py

--- scripts/critical_diagram.py
+++ scripts/critical_diagram.py
@@ -19,10 +19,16 @@ if __name__ == "__main__":
     rmse_tables = [
         (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_DecisionTree_regressor', 'RMSE_table.csv'), str(n_bins) + ' T Test RMSE', 'DT+'),
         (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_DecisionTree_regressor', 'RMSE_table.csv'), 'Base Test RMSE', 'DT'),
+
         (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_LinearRegression_regressor', 'RMSE_table.csv'), str(n_bins) + ' T Test RMSE', 'LR+'),
         (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_LinearRegression_regressor', 'RMSE_table.csv'), 'Base Test RMSE', 'LR'),
+
         (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_RandomForest_regressor', 'RMSE_table.csv'), str(n_bins) + ' T Test RMSE', 'RF+'),
         (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_RandomForest_regressor', 'RMSE_table.csv'), 'Base Test RMSE', 'RF'),
+
+        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_XGBoost_regressor', 'RMSE_table.csv'), str(n_bins) + ' T Test RMSE', 'XGB+'),
+        (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_RandomForest_classifier_XGBoost_regressor', 'RMSE_table.csv'), 'Base Test RMSE', 'XGB'),
+
         (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_Khiops_classifier_Khiops_regressor', 'RMSE_table.csv'), 'Base Test RMSE', 'Khiops'),
         (os.path.join('..', 'data', 'results_tables', 'equal_freq_below_threshold_Khiops_classifier_Khiops_regressor', 'RMSE_table.csv'), str(n_bins) + ' T Test RMSE', 'Khiops+')
     ]
@@ -37,10 +43,11 @@ if __name__ == "__main__":
         groups.append(list(tmp_df[column]))
 
     # Rank the data for each dataset
-    ranked_data = [list(stats.rankdata(row)) for row in np.array(groups).T]
+    transposed_groups = list(zip(*groups))
+    ranked_data = [list(stats.rankdata(row)) for row in transposed_groups]
 
-    average_ranks = [np.mean(vect) for vect in np.array(ranked_data).T]
+    average_ranks = [np.mean(vect) for vect in list(zip(*ranked_data))]
 
-    cd = Orange.evaluation.compute_CD(average_ranks, len(groups[0]))
+    cd = Orange.evaluation.compute_CD(average_ranks, len(rmse_tables))
     Orange.evaluation.graph_ranks(average_ranks, [vect[2] for vect in rmse_tables], cd=cd, width=6, textspace=1.5, cdmethod=cdmethod)
     plt.savefig('critical_diagram_' + str(n_bins) + 'bins.png')
